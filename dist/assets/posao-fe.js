7da2114c9af453a8bfa0f9c777f938aa3bf245db
ekalac1 <elza@Administrators-MacBook-Air.local>
2017-11-06 14:04:43 +0100

    some changes

--- dist/assets/posao-fe.js
+++ dist/assets/posao-fe.js
@@ -3007,6 +3007,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("posao-fe/app")["default"].create({"LOG_ACTIVE_GENERATION":true,"LOG_TRANSITIONS":true,"LOG_TRANSITIONS_INTERNAL":true,"LOG_VIEW_LOOKUPS":true,"name":"posao-fe","version":"0.0.0+"});
+  require("posao-fe/app")["default"].create({"LOG_ACTIVE_GENERATION":true,"LOG_TRANSITIONS":true,"LOG_TRANSITIONS_INTERNAL":true,"LOG_VIEW_LOOKUPS":true,"name":"posao-fe","version":"0.0.0+53b7231f"});
 }
 //# sourceMappingURL=posao-fe.map
