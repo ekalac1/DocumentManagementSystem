e1d655232d2e501f62ce6c8cae565e7fe6f05dbb
damir-dizdarevic-sn <damir.dizdarevic@sleepnumber.com>
2017-11-19 19:43:57 +0100

    Edit documents

--- dist/assets/posao-fe.js
+++ dist/assets/posao-fe.js
@@ -2818,6 +2818,6 @@ catch(err) {
 });
 
 if (!runningTests) {
-  require("posao-fe/app")["default"].create({"LOG_ACTIVE_GENERATION":true,"LOG_TRANSITIONS":true,"LOG_TRANSITIONS_INTERNAL":true,"LOG_VIEW_LOOKUPS":true,"name":"posao-fe","version":"0.0.0+fb969cc4"});
+  require("posao-fe/app")["default"].create({"LOG_ACTIVE_GENERATION":true,"LOG_TRANSITIONS":true,"LOG_TRANSITIONS_INTERNAL":true,"LOG_VIEW_LOOKUPS":true,"name":"posao-fe","version":"0.0.0+27c5cb7b"});
 }
 //# sourceMappingURL=posao-fe.map
